16bc2b8fc8d9b798845c9e27cc508b6cef0b787f
anykey <billykidd@mail.ru>
2025-02-18 21:41:13 +0300

    actions test 02

--- __tests__/actions.test.ts
+++ __tests__/actions.test.ts
@@ -1,42 +1,74 @@
 import { expect, test, describe } from '@jest/globals';
-import {
-  configureStore,
-  EnhancedStore,
-  StoreEnhancer,
-  ThunkDispatch,
-  Tuple,
-  UnknownAction
-} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { mockIngredient, mockStore } from '../src/services/mockData';
-import { rootReducer } from '../src/services/store';
+import { rootReducer, RootState } from '../src/services/store';
 import {
   addIngredient,
-  TInitialState
+  clearConstructorItems,
+  clearOrderModalData,
+  closeModal,
+  openModal,
+  
 } from '../src/services/slices/burgerAppSlice';
-import { TUserState } from '../src/services/auth/slice';
+
 
 describe('Синхронные экшены', () => {
-  let store;
+  let store: ReturnType<typeof configureStore>;
 
   beforeEach(() => {
-    // Инициализируем новый стор перед каждым тестом
     store = configureStore({
       reducer: rootReducer,
-      preloadedState: mockStore
+      preloadedState: mockStore,
     });
   });
 
   it('должен добавлять ингредиент в состояние', () => {
-
-    // Диспетчируем экшен
-    store.dispatch(addIngredient(mockIngredient));
+    
+    store.dispatch(addIngredient(mockIngredient)); // Диспетчируем экшен
 
     // Получаем текущее состояние
-    const state = store.getState();
+    const state = store.getState() as RootState; // Явно указываем тип
 
     // Проверяем, что ингредиент добавлен
-    expect(state.burger.constructorItems.ingredients).toContainEqual(
-      mockIngredient
-    );
+    expect(state.burger.constructorItems.ingredients).toContainEqual(mockIngredient);
   });
+
+  it('должен открывать модальное окно', () => {
+    store.dispatch(openModal());
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.isModalOpened).toBe(true);
+  });
+
+  it('должен закрывать модальное окно', () => {
+    store.dispatch(openModal());
+    store.dispatch(closeModal());
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.isModalOpened).toBe(false);
+  });
+
+  it('должен очищать данные модального окна заказа', () => {
+    store.dispatch(clearOrderModalData());
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.orderModalData).toBeNull();
+  });
+
+  it('должен очищать элементы конструктора', () => {
+    
+    store.dispatch(addIngredient(mockIngredient));
+    store.dispatch(clearConstructorItems());
+
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.constructorItems.ingredients).toHaveLength(0);
+    expect(state.burger.constructorItems.bun.price).toBe(0);
+  });
+
+
+
+
+
+
+
+
+
+
 });
