cef996b39cd61419fc32760115b0a9352a12368a
anykey <billykidd@mail.ru>
2025-02-25 22:40:22 +0300

    async test 02

--- __tests__/asyncActions.jest.ts
+++ __tests__/asyncActions.jest.ts
@@ -6,18 +6,18 @@ import {
   checkUserAuth,
   setUser
 } from '../src/services/auth/actions';
-import { setIsAuthChecked } from '../src/services/auth/slice';
-import {
-  getUserApi,
-  loginUserApi,
-  logoutApi,
-  registerUserApi
-} from '../src/utils/burger-api';
-import { TUser } from '../src/utils/types';
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer, RootState } from '../src/services/store';
 import { mockStore, orderMockData } from '../src/services/mockData';
-import { fetchIngredients, postNewOrder } from '../src/services/slices/burgerAppSlice';
+import {
+  fetchFeed,
+  fetchIngredients,
+  fetchOrder,
+  fetchUserOrders,
+  postNewOrder
+} from '../src/services/slices/burgerAppSlice';
+import { TOrder } from '../src/utils/types';
+import { getOrderByNumberApi } from '../src/utils/burger-api';
 
 describe('burgerAppSlice async actions', () => {
   let store: ReturnType<typeof configureStore>;
@@ -29,6 +29,8 @@ describe('burgerAppSlice async actions', () => {
     });
   });
 
+  // INGREDIENTS //
+
   it('should handle fetchIngredients.pending', async () => {
     await store.dispatch(fetchIngredients.pending('pending'));
     const state = store.getState() as RootState; // Явно указываем тип
@@ -37,8 +39,10 @@ describe('burgerAppSlice async actions', () => {
 
   it('should handle fetchIngredients.fulfilled', async () => {
     const state = store.getState() as RootState; // Явно указываем тип
-    const mockIngredients = state.burger.ingredients
-    await store.dispatch(fetchIngredients.fulfilled(mockIngredients, 'fulfilled'));
+    const mockIngredients = state.burger.ingredients;
+    await store.dispatch(
+      fetchIngredients.fulfilled(mockIngredients, 'fulfilled')
+    );
     expect(state.burger.loading).toBe(false);
     expect(state.burger.ingredients).toEqual(mockIngredients);
   });
@@ -50,8 +54,12 @@ describe('burgerAppSlice async actions', () => {
     expect(state.burger.loading).toBe(false);
   });
 
+  // NEW ORDER //
+
   it('should handle postNewOrder.pending', async () => {
-    await store.dispatch(postNewOrder.pending('pending', orderMockData.ingredients));
+    await store.dispatch(
+      postNewOrder.pending('pending', orderMockData.ingredients)
+    );
     const state = store.getState() as RootState; // Явно указываем тип
     expect(state.burger.orderRequest).toBe(true);
   });
@@ -60,7 +68,7 @@ describe('burgerAppSlice async actions', () => {
     const mockResponse = {
       success: true,
       name: 'testname',
-      order:   {
+      order: {
         ingredients: [
           '643d69a5c3f7b9001cfa093d',
           '643d69a5c3f7b9001cfa0941',
@@ -74,20 +82,148 @@ describe('burgerAppSlice async actions', () => {
         number: 123
       }
     };
-    await store.dispatch(postNewOrder.fulfilled(mockResponse, 'fulfilled', orderMockData.ingredients));
+    await store.dispatch(
+      postNewOrder.fulfilled(
+        mockResponse,
+        'fulfilled',
+        orderMockData.ingredients
+      )
+    );
     const state = store.getState() as RootState; // Явно указываем тип
     expect(state.burger.orderModalData).toEqual(orderMockData);
     expect(state.burger.orderRequest).toBe(false);
   });
 
   it('should handle postNewOrder.rejected', () => {
-    const state = store.getState() as RootState; // Явно указываем тип
     const mockAnswer = { name: 'test', message: 'error' };
     store.dispatch(postNewOrder.rejected(mockAnswer, 'rejected', ['']));
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(false);
+  });
+
+  // FEED //
+
+  it('should handle fetchFeed.pending', async () => {
+    await store.dispatch(fetchFeed.pending('pending'));
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(true);
+  });
+
+  it('should handle fetchFeed fulfilled', async () => {
+    const mockResponse = {
+      success: true,
+      total: 100,
+      totalToday: 10,
+      orders: [
+        {
+          _id: '664e927097ede0001d06bdb9',
+          ingredients: [
+            '643d69a5c3f7b9001cfa093d',
+            '643d69a5c3f7b9001cfa093e',
+            '643d69a5c3f7b9001cfa093d'
+          ],
+          status: 'done',
+          name: 'Флюоресцентный люминесцентный бургер',
+          createdAt: '2024-05-23T00:48:48.039Z',
+          updatedAt: '2024-05-23T00:48:48.410Z',
+          number: 40680
+        }
+      ]
+    };
+
+    await store.dispatch(fetchFeed.fulfilled(mockResponse, 'fulfilled'));
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(false);
+    expect(state.burger.feed.orders).toEqual(mockResponse.orders);
+  });
+
+  it('should handle fetchFeed.rejected', () => {
+    const mockAnswer = { name: 'test', message: 'error' };
+    store.dispatch(fetchFeed.rejected(mockAnswer, 'rejected'));
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(false);
+  });
+
+  // USER ORDERS //
+
+  it('should handle fetchUserOrders.pending', async () => {
+    await store.dispatch(fetchUserOrders.pending('pending'));
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(true);
+  });
+
+  it('should handle fetchUserOrders.fulfilled', async () => {
+    const mockResponse = [
+      {
+        _id: '664e927097ede0001d06bdb9',
+        ingredients: [
+          '643d69a5c3f7b9001cfa093d',
+          '643d69a5c3f7b9001cfa093e',
+          '643d69a5c3f7b9001cfa093d'
+        ],
+        status: 'done',
+        name: 'Флюоресцентный люминесцентный бургер',
+        createdAt: '2024-05-23T00:48:48.039Z',
+        updatedAt: '2024-05-23T00:48:48.410Z',
+        number: 40680
+      }
+    ];
+
+    await store.dispatch(fetchUserOrders.fulfilled(mockResponse, 'fulfilled'));
+    const state = store.getState() as RootState; // Явно указываем тип
+
+    expect(state.burger.loading).toBe(false);
+    expect(state.burger.userOrders).toEqual(mockResponse);
+  });
+
+  it('should handle fetchUserOrders.rejected', () => {
+    const mockAnswer = { name: 'test', message: 'error' };
+    store.dispatch(fetchUserOrders.rejected(mockAnswer, 'rejected'));
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(false);
+  });
+
+  // ORDER BY NUMBER //
+
+  it('should handle fetchOrder.pending', async () => {
+    await store.dispatch(fetchOrder.pending('', 123, 'pending'));
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(true);
+  });
+
+  it('should handle fetchOrder.fulfilled', async () => {
+    const mockResponse: TOrder = {
+      _id: '664e927097ede0001d06bdb9',
+      ingredients: [
+        '643d69a5c3f7b9001cfa093d',
+        '643d69a5c3f7b9001cfa093e',
+        '643d69a5c3f7b9001cfa093d'
+      ],
+      status: 'done',
+      name: 'Флюоресцентный люминесцентный бургер',
+      createdAt: '2024-05-23T00:48:48.039Z',
+      updatedAt: '2024-05-23T00:48:48.410Z',
+      number: 40680
+    };
+
+    await store.dispatch(
+      fetchOrder.fulfilled(mockResponse, 'fulfilled', 40680)
+    );
+    const state = store.getState() as RootState; // Явно указываем тип
+    expect(state.burger.loading).toBe(false);
+    expect(state.burger.orderModalData).toEqual(mockResponse);
+  });
+
+  it('should handle fetchOrder.rejected', () => {
+    const mockAnswer = { name: 'test', message: 'error' };
+    store.dispatch(fetchOrder.rejected(mockAnswer, 'rejected', 40680));
+    const state = store.getState() as RootState; // Явно указываем тип
     expect(state.burger.loading).toBe(false);
   });
 
 
 
 
+
+  
 });
